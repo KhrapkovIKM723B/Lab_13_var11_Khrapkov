312a6efa5b040f4db55a524b2fcf33d7406b9d90
KhrapkovIKM723B <Ihor.Khrapkov@infiz.khpi.edu.ua>
2024-04-19 00:41:23 +0300

    First commit

---
+++ Lab_13_var11_Khrapkov.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <iomanip> 
+using namespace std;
+
+double term(int n)
+{
+    if (n == 1)
+    {
+        return 3.0;
+    }
+    else {
+        return term(n - 1) + 0.3;
+    }
+}
+
+int main() 
+{
+    int n;
+    cout << "Enter n!: ";
+    cin >> n;
+
+    if (n < 1 || n > 11)
+    {
+        cout << "Please enter a term number from 1 to 11." << endl;
+        return 1;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << "! = " << fixed << setprecision(1) << term(i) << endl;
+    }
+
+    return 0;
+}
